5f3e7587411a79fab9f287a7258b63f992de5169
BeauteousJade <pby2196995023@163.com>
2021-03-28 17:32:22 +0800

    增加LivaData的测试代码

--- app/src/main/java/com/jade/kotlindemo/page/room/RoomActivity.kt
+++ app/src/main/java/com/jade/kotlindemo/page/room/RoomActivity.kt
@@ -1,10 +1,10 @@
 package com.jade.kotlindemo.page.room
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import androidx.room.Room
 import com.jade.kotlindemo.R
 import com.jade.kotlindemo.helper.DataBaseHelper
 import com.jade.kotlindemo.page.room.bean.User
@@ -31,6 +31,30 @@ class RoomActivity : AppCompatActivity() {
             lifecycleScope.launch(Dispatchers.IO) {
                 DataBaseHelper.dataBase?.run {
                     userDao().clearUser()
+                }
+            }
+        }
+        findViewById<View>(R.id.update_user).setOnClickListener {
+            lifecycleScope.launch(Dispatchers.IO) {
+                val userLiveData = DataBaseHelper.dataBase.userDao().getUserByIdV2("1")
+                lifecycleScope.launch(Dispatchers.Main) {
+                    userLiveData.observe(this@RoomActivity) {
+                        Log.i("pby123", "observe user = $it")
+                    }
+                }
+                // liveData 赋值时时通过postValue实现的，这里直接获取value是为空，
+                // 所以先delay 100ms,保证先更新完值，才去获取正确的值
+                delay(100)
+                val oldUser = userLiveData.value
+                Log.i("pby123", "user = $oldUser")
+                oldUser?.let {
+                    val newUser = User(
+                        userId = oldUser.userId,
+                        name = "new pby",
+                        age = oldUser.age,
+                        sex = oldUser.sex
+                    )
+                    DataBaseHelper.dataBase.userDao().updateUser(newUser)
                 }
             }
         }
--- app/src/main/java/com/jade/kotlindemo/page/room/dao/UserDao.kt
+++ app/src/main/java/com/jade/kotlindemo/page/room/dao/UserDao.kt
@@ -1,5 +1,6 @@
 package com.jade.kotlindemo.page.room.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.jade.kotlindemo.page.room.bean.User
 import com.jade.kotlindemo.page.room.bean.UserAndLibrary
@@ -12,6 +13,12 @@ interface UserDao {
 
     @Query("select * from user where user_id = :userId")
     fun getUserById(userId: String): User
+
+    @Query("select * from user where user_id = :userId")
+    fun getUserByIdV2(userId: String): LiveData<User>
+
+    @Update
+    fun updateUser(user: User)
 
     @Delete
     fun delete(user: User)
