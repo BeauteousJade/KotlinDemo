b8a9f22ea7c4b5d6efee87e0d2d0b167355badf6
BeauteousJade <pby2196995023@163.com>
2021-05-07 03:22:13 +0800

    MyConstraintLayout 替换成Java书写

--- app/src/main/java/com/jade/kotlindemo/page/constraint/MyConstraintLayout.java
+++ app/src/main/java/com/jade/kotlindemo/page/constraint/MyConstraintLayout.java
@@ -8,24 +8,24 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-public class MyConstraintLayoutV2 extends ConstraintLayout {
+public class MyConstraintLayout extends ConstraintLayout {
 
     private int mOnMeasureWidthMeasureSpec = 0;
     private int mOnMeasureHeightMeasureSpec = 0;
 
-    public MyConstraintLayoutV2(@NonNull Context context) {
+    public MyConstraintLayout(@NonNull Context context) {
         super(context);
     }
 
-    public MyConstraintLayoutV2(@NonNull Context context, @Nullable AttributeSet attrs) {
+    public MyConstraintLayout(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public MyConstraintLayoutV2(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public MyConstraintLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
-    public MyConstraintLayoutV2(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    public MyConstraintLayout(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
 
