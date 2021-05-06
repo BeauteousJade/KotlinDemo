2d37f58e1b53a17353c9f8036e0b1df70879f0b3
BeauteousJade <pby2196995023@163.com>
2021-05-07 03:18:22 +0800

    Copy ConstraintLayout 2.0.4版本的源码

---
+++ app/src/main/java/com/jade/kotlindemo/page/constraint/MyConstraintLayoutV2.java
@@ -0,0 +1,81 @@
+package com.jade.kotlindemo.page.constraint;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class MyConstraintLayoutV2 extends ConstraintLayout {
+
+    private int mOnMeasureWidthMeasureSpec = 0;
+    private int mOnMeasureHeightMeasureSpec = 0;
+
+    public MyConstraintLayoutV2(@NonNull Context context) {
+        super(context);
+    }
+
+    public MyConstraintLayoutV2(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyConstraintLayoutV2(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public MyConstraintLayoutV2(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        if (skipMeasure(widthMeasureSpec, heightMeasureSpec)) {
+            return;
+        }
+        mOnMeasureWidthMeasureSpec = widthMeasureSpec;
+        mOnMeasureHeightMeasureSpec = heightMeasureSpec;
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    private boolean skipMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (mDirtyHierarchy) {
+            return false;
+        }
+        final int childCount = getChildCount();
+        for (int index = 0; index < childCount; index++) {
+            View child = getChildAt(index);
+            if (child.isLayoutRequested() && !(child.getMeasuredHeight() > 0 && child.getMeasuredWidth() > 0)) {
+                return false;
+            }
+        }
+        if (mOnMeasureWidthMeasureSpec == widthMeasureSpec && mOnMeasureHeightMeasureSpec == heightMeasureSpec) {
+            resolveMeasuredDimension(widthMeasureSpec, heightMeasureSpec, mLayoutWidget.getWidth(), mLayoutWidget.getHeight(), mLayoutWidget.isWidthMeasuredTooSmall(), mLayoutWidget.isHeightMeasuredTooSmall());
+            return true;
+        }
+        if (mOnMeasureWidthMeasureSpec == widthMeasureSpec && MeasureSpec.getMode(
+                widthMeasureSpec
+        ) == MeasureSpec.EXACTLY && MeasureSpec.getMode(heightMeasureSpec) == MeasureSpec.AT_MOST && MeasureSpec.getMode(
+                mOnMeasureHeightMeasureSpec
+        ) == MeasureSpec.AT_MOST
+        ) {
+            int newSize = MeasureSpec.getSize(heightMeasureSpec);
+            if (newSize >= mLayoutWidget.getHeight()) {
+                mOnMeasureWidthMeasureSpec = widthMeasureSpec;
+                mOnMeasureHeightMeasureSpec = heightMeasureSpec;
+                resolveMeasuredDimension(
+                        widthMeasureSpec,
+                        heightMeasureSpec,
+                        mLayoutWidget.getWidth(),
+                        mLayoutWidget.getHeight(),
+                        mLayoutWidget.isWidthMeasuredTooSmall(),
+                        mLayoutWidget.isHeightMeasuredTooSmall()
+                );
+                return true;
+            }
+        }
+        return false;
+    }
+}
