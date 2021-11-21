1c4505f8916c7d6a3c016dd817741f816bfaaed6
BeauteousJade <pby2196995023@163.com>
2021-11-21 17:57:14 +0800

    增加进入和推出的动画效果

--- app/src/main/java/com/jade/kotlindemo/page/nav/NavActivity.kt
+++ app/src/main/java/com/jade/kotlindemo/page/nav/NavActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import com.jade.kotlindemo.R
 
@@ -12,8 +13,12 @@ class NavActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nav)
+        // 使用这种方式，Fragment不能返回了，以后看看原因
         val navHostFragment = NavHostFragment.create(R.navigation.nav_graph, bundleOf("pby" to "1"))
         val view = findViewById<View>(R.id.nav_host_fragment)
-        supportFragmentManager.beginTransaction().add(view.id, navHostFragment).commit()
+        supportFragmentManager.beginTransaction()
+//            .add(view.id, navHostFragment, null)
+//            .setReorderingAllowed(true)
+//            .commitNowAllowingStateLoss()
     }
 }
--- app/src/main/java/com/jade/kotlindemo/page/nav/NavSecondActivity.kt
+++ app/src/main/java/com/jade/kotlindemo/page/nav/NavSecondActivity.kt
@@ -1,8 +1,12 @@
 package com.jade.kotlindemo.page.nav
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ActivityNavigator
 
 class NavSecondActivity : AppCompatActivity() {
 
-
+    override fun finish() {
+        super.finish()
+        ActivityNavigator.applyPopAnimationsToPendingTransition(this)
+    }
 }
