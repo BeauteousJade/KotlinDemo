742499291661c5a4b54e07a69030687ee0b2145a
BeauteousJade <pby2196995023@163.com>
2022-04-17 20:24:53 +0800

    引入MMKV

--- app/src/main/java/com/jade/kotlindemo/App.kt
+++ app/src/main/java/com/jade/kotlindemo/App.kt
@@ -1,11 +1,15 @@
 package com.jade.kotlindemo
 
 import android.app.Application
+import com.tencent.mmkv.MMKV
+
 
 class App : Application() {
 
     override fun onCreate() {
         super.onCreate()
         System.setProperty("kotlinx.coroutines.debug", "on")
+        val rootDir = MMKV.initialize(this)
+        println("mmkv root: $rootDir")
     }
 }
